a13bcbc8940c357b59f669a0bc17946f8ba72b86
onishi <h520083m@mails.cc.ehime-u.ac.jp>
2024-06-30 19:49:16 +0900

    tree-sitterを使ったJavaMethodの抜き出しプログラムの作成

---
+++ code/test.java
@@ -0,0 +1,20 @@
+package tool;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * ドックストリング
+ */
+public class Test {
+    public static void main(String[] args) {
+        // forループを使用して3回Hello Worldを出力
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello World!");
+        }
+    }
+    public static void sayHello() {
+        System.out.println("Hello!");
+    }
+}
